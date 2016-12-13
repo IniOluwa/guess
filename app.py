faf68626b635ab257c7c3196d1809e07d1120333
IniOluwa <inioluwafageyinbo@gmail.com>
2016-12-13 04:58:12 +0100

    Question template created

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+questions = ['Are they ninjas?', 'Do they have Dragon Balls?', 'Are they souls?', 'Do they have devil fruits?', 'Are they Mages?']
 guesses = ['Naruto', 'One Piece', 'Bleach', 'Fairytail', 'DBS']
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/question/<int:id>')
+def question(id):
+    return render_template('question.html', question=questions[id])
 
 @app.route('/guess/<int:id>')
 def guess(id):
